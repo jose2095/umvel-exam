93eef30f5ecce1d8901f4ae1a4e499f83544142f
Jose <33932651+jose2095@users.noreply.github.com>
2021-11-03 15:40:00 -0600

    style(users): change design users

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -6,6 +6,9 @@ import { UsersComponent } from './users/users.component';
 //material imports
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { ComponentsModule } from 'src/app/components/components.module';
     CommonModule,
     AdminRoutingModule,
     MatSidenavModule,
-    ComponentsModule
+    ComponentsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AdminModule { }
